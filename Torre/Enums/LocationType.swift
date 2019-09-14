710ac7d2022414017e298aa744d60b67642d58d1
Fernando Gallo <fernandgallo@gmail.com>
2019-09-14 14:37:08 -0300

    LocationType enum

---
+++ Torre/Enums/LocationType.swift
@@ -0,0 +1,18 @@
+//
+//  LocationType.swift
+//  Torre
+//
+//  Created by Fernando Gallo on 14/09/19.
+//  Copyright © 2019 Fernando Gallo. All rights reserved.
+//
+
+import Foundation
+
+enum LocationType: String {
+    case city
+    case group
+    case landmark
+    case metro
+    case subzone
+    case zone
+}

b489f63d85dce66efb79748fb5bb375005d30a2c
Fernando Gallo <fernandgallo@gmail.com>
2019-09-14 13:07:19 -0300

    Initial commit

---
+++ Torre/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Torre
+//
+//  Created by Fernando Gallo on 14/09/19.
+//  Copyright © 2019 Fernando Gallo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TorreTests/TorreTests.swift
@@ -0,0 +1,34 @@
+//
+//  TorreTests.swift
+//  TorreTests
+//
+//  Created by Fernando Gallo on 14/09/19.
+//  Copyright © 2019 Fernando Gallo. All rights reserved.
+//
+
+import XCTest
+@testable import Torre
+
+class TorreTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

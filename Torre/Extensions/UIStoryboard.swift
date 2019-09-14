182674b5ed717b2c4ebb4ea401a40478599a2799
Fernando Gallo <fernandgallo@gmail.com>
2019-09-14 13:28:14 -0300

    UIStoryboard extension

---
+++ Torre/Extensions/UIStoryboard.swift
@@ -0,0 +1,25 @@
+//
+//  UIStoryboard.swift
+//  Torre
+//
+//  Created by Fernando Gallo on 14/09/19.
+//  Copyright © 2019 Fernando Gallo. All rights reserved.
+//
+
+import UIKit
+
+extension UIStoryboard {
+    
+    @nonobjc static var main: UIStoryboard {
+        return UIStoryboard(name: "Main", bundle: nil)
+    }
+    
+    var restaurantsViewController: RestaurantsViewController {
+        guard let viewController =
+            UIStoryboard.main.instantiateViewController(withIdentifier: "RestaurantsViewController") as? RestaurantsViewController else {
+                fatalError("RestaurantsViewController couldn't be found in Storyboard file")
+        }
+        return viewController
+    }
+    
+}
